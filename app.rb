a23d77f58eb3a31ea3666e7b33edf31935fc47bb
Rikuoh <mail@riq0h.jp>
2024-08-18 15:21:44 +0900

    SQL文の変更、UUID周りの処理をDBに一任、DBアクセス処理のメソッド化

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
-require 'securerandom'
 require 'cgi'
 
 configure do
@@ -18,6 +17,32 @@ helpers do
   def db
     settings.conn
   end
+
+  def find_memo(id)
+    db.exec_params('SELECT * FROM memos WHERE id = $1', [id]).first
+  end
+
+  def create_memo(title, content)
+    db.exec_params(
+      'INSERT INTO memos (title, content) VALUES ($1, $2) RETURNING id',
+      [title, content]
+    ).first['id']
+  end
+
+  def update_memo(id, title, content)
+    db.exec_params(
+      'UPDATE memos SET title = $1, content = $2 WHERE id = $3',
+      [title, content, id]
+    )
+  end
+
+  def delete_memo(id)
+    db.exec_params('DELETE FROM memos WHERE id = $1', [id])
+  end
+
+  def all_memos
+    db.exec('SELECT * FROM memos ORDER BY created_at DESC').to_a
+  end
 end
 
 get '/' do
@@ -25,7 +50,7 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = db.exec('SELECT * FROM memos').to_a
+  @memos = all_memos
   erb :index
 end
 
@@ -34,36 +59,29 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  uuid = SecureRandom.uuid
-  db.exec_params(
-    'INSERT INTO memos (id, title, content) VALUES ($1, $2, $3)',
-    [uuid, params[:title], params[:content]]
-  )
-  redirect '/memos'
+  id = create_memo(params[:title], params[:content])
+  redirect "/memos/#{id}"
 end
 
 get '/memos/:id' do
-  @memo = db.exec_params('SELECT * FROM memos WHERE id = $1', [params[:id]]).first
+  @memo = find_memo(params[:id])
   halt 404, erb(:not_found) unless @memo
   erb :show
 end
 
 get '/memos/:id/edit' do
-  @memo = db.exec_params('SELECT * FROM memos WHERE id = $1', [params[:id]]).first
+  @memo = find_memo(params[:id])
   halt 404, erb(:not_found) unless @memo
   erb :edit
 end
 
 patch '/memos/:id' do
-  db.exec_params(
-    'UPDATE memos SET title = $1, content = $2 WHERE id = $3',
-    [params[:title], params[:content], params[:id]]
-  )
+  update_memo(params[:id], params[:title], params[:content])
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  db.exec_params('DELETE FROM memos WHERE id = $1', [params[:id]])
+  delete_memo(params[:id])
   redirect '/memos'
 end
 
